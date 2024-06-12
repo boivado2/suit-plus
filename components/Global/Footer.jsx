296d394ddd1d72c93e009a1d4ff9db5b0692b0fc
Hariz-Ade14 <sannihariz14@gmail.com>
2024-06-11 21:09:52 -0700

    started working on the trends section

---
+++ components/Global/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<div className='container-fluid bg-blue-950 h-100'>
+			{/* <div className='footer row'>
+				<div className='col'>
+					<h3>SuitPlus</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+						itaque repellat sit ullam cumque voluptate non rem, nulla magnam
+						tempora recusandae deleniti perferendis id corrupti dolore minus
+						quam qui consectetur.
+					</p>
+				</div>
+			</div> */}
+		</div>
+	);
+}
+
+export default Footer;
